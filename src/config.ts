12c1c45075b34cfe2037e78bcc20270efdc4b289
Fabio <fabiodn.798@gmail.com>
2023-03-13 20:52:31 +0100

    ⚡️ Create config file

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const config = {
+  user: process.env.USERNAME,
+  pass: process.env.PASSWORD,
+  cluster: process.env.CLUSTER,
+  dbname: process.env.NAME,
+  jwtsecret: process.env.SECRET,
+};

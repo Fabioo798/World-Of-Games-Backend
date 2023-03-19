0ff873a19fb245b85c478bfb4528951456c9ef65
Fabio <fabiodn.798@gmail.com>
2023-03-19 14:18:20 +0100

    🏗️ Create user controller

---
+++ src/server/application/controllers/user.controller.test.ts
@@ -0,0 +1,229 @@
+import { NextFunction, Request, Response } from 'express';
+import { RequestPlus, TokenPayload } from '../../../commons/interfaces.js';
+import UserCreator from '../../../user/application/usercreator.js';
+import UserDeleter from '../../../user/application/userdeleter.js';
+import UserFinder from '../../../user/application/userfinder.js';
+import UserSearcher from '../../../user/application/usersearcher.js';
+import UserUpdater from '../../../user/application/userupdater.js';
+import UserRepository from '../../../user/domain/user.model.repo.js';
+import { Auth } from '../auth/auth.js';
+import { UserController } from './user.controller.js';
+
+jest.mock('../auth/auth.js');
+jest.mock('../../../config.js', () => ({
+  _dirname: 'test',
+  config: {
+    secret: 'test',
+  },
+}));
+
+const res = {
+  json: jest.fn(),
+  status: jest.fn(),
+} as unknown as Response;
+
+const next = jest.fn() as unknown as NextFunction;
+
+const pass = 'test1';
+
+const mockRepo = {
+  create: jest.fn(),
+  search: jest.fn(),
+  find: jest.fn(),
+  update: jest.fn(),
+  delete: jest.fn(),
+} as unknown as UserRepository;
+
+const mockuserCreator = new UserCreator(mockRepo);
+const mockuserSearcher = new UserSearcher(mockRepo);
+const mockuserFinder = new UserFinder(mockRepo);
+const mockuserUpdate = new UserUpdater(mockRepo);
+const mockuserDeleter = new UserDeleter(mockRepo);
+
+describe('Given userController class', () => {
+  const controller = new UserController(
+    mockuserCreator,
+    mockuserFinder,
+    mockuserSearcher,
+    mockuserUpdate,
+    mockuserDeleter
+  );
+  describe('When it is instanced', () => {
+    test('Then it should call the userController', () => {
+      expect(controller).toBeInstanceOf(UserController);
+    });
+  });
+
+  describe('When findUser method is called', () => {
+    test('Then if the user info is completed it should return the status and json', async () => {
+      const req = {
+        params: {
+          id: '1',
+        },
+      } as unknown as Request;
+
+      await controller.findUser(req, res, next);
+      expect(res.status).toHaveBeenCalled();
+      expect(res.json).toHaveBeenCalled();
+    });
+    test('Then if the user info is INcompleted it should return next', async () => {
+      const req = {} as unknown as Request;
+
+      await controller.findUser(req, res, next);
+      expect(next).toHaveBeenCalled();
+    });
+  });
+
+  describe('When registerUser method is called', () => {
+    test('Then if the user information is completed, it should return the resp.satus and resp.json', async () => {
+      const req = {
+        body: {
+          name: 'test',
+          email: 'test',
+          password: pass,
+          img: 'test',
+        },
+      } as unknown as Request;
+
+      await controller.registerUser(req, res, next);
+      expect(res.status).toHaveBeenCalled();
+      expect(res.json).toHaveBeenCalled();
+    });
+
+    test('Then if user information in the body, has not user name, it should be catch the error and next function have been called', async () => {
+      const req = {
+        body: {
+          password: 'test',
+        },
+      } as unknown as Request;
+
+      await controller.registerUser(req, res, next);
+      expect(next).toHaveBeenCalled();
+    });
+  });
+
+  describe('When loginUser method is called', () => {
+    test('Then if the user information is completed, it should return the resp.status and resp.json', async () => {
+      const req = {
+        body: {
+          email: 'test',
+          password: pass,
+        },
+      } as unknown as Request;
+
+      (mockRepo.search as jest.Mock).mockResolvedValue(['test']);
+
+      Auth.compare = jest.fn().mockResolvedValue(true);
+
+      await controller.loginUser(req, res, next);
+      expect(res.status).toHaveBeenCalled();
+      expect(res.json).toHaveBeenCalled();
+    });
+
+    test('Then if the user information has not email, it should be catch the error and next function have been called', async () => {
+      const req = {
+        body: {
+          password: pass,
+        },
+      } as unknown as Request;
+
+      await controller.loginUser(req, res, next);
+      expect(next).toHaveBeenCalled();
+    });
+
+    test('Then if the user information has not password, it should be catch the error and next function have been called', async () => {
+      const req = {
+        body: {
+          email: 'test',
+        },
+      } as unknown as Request;
+
+      await controller.loginUser(req, res, next);
+      expect(next).toHaveBeenCalled();
+    });
+
+    test('Then if the user information is complete but the search method return an empty array, it should be catch the error and next function have been called', async () => {
+      const req = {
+        body: {
+          email: 'test',
+          password: pass,
+        },
+      } as unknown as Request;
+
+      (mockRepo.search as jest.Mock).mockResolvedValue([]);
+
+      await controller.loginUser(req, res, next);
+      expect(next).toHaveBeenCalled();
+    });
+
+    test('Then if the user information is complete but the compare method of Auth return false, it should be catch the error and next function have been called', async () => {
+      const req = {
+        body: {
+          email: 'test',
+          password: pass,
+        },
+      } as unknown as Request;
+
+      (mockRepo.search as jest.Mock).mockResolvedValue(['test']);
+
+      Auth.compare = jest.fn().mockResolvedValue(false);
+
+      await controller.loginUser(req, res, next);
+      expect(next).toHaveBeenCalled();
+    });
+  });
+
+  describe('When call the updateUser method', () => {
+    describe('When all params are correct', () => {
+      test('Then it should call resp.json', async () => {
+        const req = {
+          body: { name: 'test', email: 'Test', password: 'test' },
+          info: { id: '1' },
+          params: { id: '1' },
+        } as unknown as RequestPlus;
+        (mockRepo.update as jest.Mock).mockResolvedValue({ id: '1' });
+        await controller.updateUser(req, res, next);
+        expect(res.json).toHaveBeenCalled();
+      });
+    });
+
+    describe('When updateUser fails', () => {
+      test('Then it should call next', async () => {
+        const req = {
+          body: { name: 'test', email: 'Test', password: 'test' },
+          info: { id: '1' },
+        } as unknown as RequestPlus;
+        (mockRepo.find as jest.Mock).mockResolvedValue(undefined);
+
+        await controller.updateUser(req, res, next);
+        expect(next).toHaveBeenCalled();
+      });
+    });
+
+    describe('When there is no req.info.id', () => {
+      test('Then it should call next', async () => {
+        const req = {
+          body: { name: 'test', email: 'Test', password: 'test' },
+          info: { id: '1' },
+        } as unknown as RequestPlus;
+        req.info = undefined as unknown as TokenPayload;
+        await controller.updateUser(req, res, next);
+        expect(next).toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe('When deleteUser method is called', () => {
+    test('Then if the id is found it should respond with status', async () => {
+      const req = {
+        body: { name: 'test', email: 'Test', password: 'test' },
+        info: { id: '1' },
+        params: { id: '1' },
+      } as unknown as RequestPlus;
+
+      (mockRepo.delete as jest.Mock).mockResolvedValue({ id: '1' });
+      await controller.deleteUser(req, res, next);
+      expect(res.status).toHaveBeenCalled();
+    });
+  });
+});
--- src/server/application/controllers/user.controller.ts
+++ src/server/application/controllers/user.controller.ts
@@ -1,1 +1,139 @@
+import createDebug from 'debug';
+import { NextFunction, Request, Response } from 'express';
+import { HTTPError } from '../../../commons/error.js';
+import { TokenPayload } from '../../../commons/interfaces.js';
+import UserCreator from '../../../user/application/usercreator.js';
+import UserDeleter from '../../../user/application/userdeleter.js';
+import UserFinder from '../../../user/application/userfinder.js';
+import UserSearcher from '../../../user/application/usersearcher.js';
+import UserUpdater from '../../../user/application/userupdater.js';
+import { Auth } from '../auth/auth.js';
 
+
+const debug = createDebug('WOF: user controller');
+
+export class UserController {
+  // eslint-disable-next-line max-params
+  constructor(
+    private userCreator: UserCreator,
+    private userFinder: UserFinder,
+    private userSearcher: UserSearcher,
+    private userUpdater: UserUpdater,
+    private userDeleter: UserDeleter
+  ) {
+    debug('User controller instantiated...');
+  }
+
+  async findUser(req: Request, res: Response, next: NextFunction) {
+    try {
+      debug('findUser')
+      const { id } = req.params;
+      const response = await this.userFinder.execute(id);
+
+      res.status(200);
+      res.json({results: [response]});
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async registerUser(req: Request, res: Response, next: NextFunction) {
+    try {
+      debug('registerUser')
+      const { body } = req;
+
+      if (
+        !req.body.email ||
+        !req.body.password ||
+        !req.body.name ||
+        !req.body.img
+      )
+        throw new HTTPError(
+          401,
+          'Unauthorized',
+          'Invalid Name, Email or password'
+        );
+
+      req.body.password = await Auth.hash(req.body.password);
+
+      req.body.cart = [];
+      req.body.myGames = [];
+
+      const data = await this.userCreator.execute(body);
+
+      res.status(201);
+      res.json({
+        results: [data],
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async loginUser(req: Request, res: Response, next: NextFunction) {
+    try {
+      debug('loginUser')
+      if (!req.body.email || !req.body.password)
+        throw new HTTPError(401, 'Unauthorized', 'Invalid Email or password');
+
+      const data = await this.userSearcher.execute({
+        key: 'email',
+        value: req.body.email,
+      });
+
+      if (!data.length)
+        throw new HTTPError(401, 'Unauthorized', 'username not found');
+
+      if (!(await Auth.compare(req.body.password, data[0].password)))
+        throw new HTTPError(401, 'Unauthorized', 'Password not match');
+
+      const payload: TokenPayload = {
+        id: data[0].id,
+        email: data[0].email,
+        role: 'Admin',
+      };
+
+      const token = Auth.createJWT(payload);
+      res.status(202);
+      res.json({
+        results: {
+          token,
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async updateUser(req: Request, res: Response, next: NextFunction) {
+    try {
+      debug('updateUser')
+      const { id } = req.params;
+      const { body } = req;
+
+      const newUser = {
+        id,
+        ...body,
+      };
+
+      await this.userUpdater.execute(newUser);
+
+      res.sendStatus(200);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async deleteUser(req: Request, res: Response, next: NextFunction){
+    try {
+      debug('deleteUser')
+      const { id } = req.params;
+
+      await this.userDeleter.execute(id);
+
+      res.sendStatus(204);
+    } catch (error) {
+      next(error);
+    }
+  }
+}

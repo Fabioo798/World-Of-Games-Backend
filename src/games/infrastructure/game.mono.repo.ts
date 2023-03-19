69053540a58aa6f0ef93abd758e3c7d069d5f31e
Fabio <fabiodn.798@gmail.com>
2023-03-19 14:21:20 +0100

    ✅ Create game mongo repo

---
+++ src/games/infrastructure/game.mono.repo.ts
@@ -0,0 +1,40 @@
+import { GameModel } from '../../server/domain/game.schema.js';
+import Game from '../domain/game.model.js';
+import GameRepository from '../domain/game.repo.model.js';
+
+export default class GameMongoRepo implements GameRepository {
+  private gameModel: typeof GameModel;
+
+  constructor(gameModel: typeof GameModel) {
+    this.gameModel = gameModel;
+  }
+
+  async create(game: Game): Promise<Game> {
+    const data = await this.gameModel.create(game);
+    return data;
+  }
+
+  async update(game: Partial<Game>): Promise<void> {
+    await this.gameModel.findByIdAndUpdate(game.id, game);
+  }
+
+  async findAll(): Promise<Game[]> {
+    const response = await this.gameModel.find();
+    return response;
+  }
+
+  async findOne(id: string): Promise<Game | null> {
+    const response = await this.gameModel.findById(id);
+    return response;
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.gameModel.findByIdAndDelete(id);
+  }
+
+  async search(query: { key: string; value: unknown }): Promise<Game[]> {
+    const response = await this.gameModel.find({ [query.key]: query.value });
+
+    return response;
+  }
+}
